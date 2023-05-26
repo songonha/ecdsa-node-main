e0da32716f5e76d76f8a6ec48b313a5a0dbdb935
Songonha <105191398+songonha@users.noreply.github.com>
2023-05-26 23:09:12 +0700

    Update server.js

--- client/src/server.js
+++ client/src/server.js
@@ -4,10 +4,10 @@ const server = axios.create({
   baseURL: "http://localhost:3042",
   withCredentials: false,
   headers: {
-          'Access-Control-Allow-Origin' : '*',
-          'Access-Control-Allow-Headers': '*',
-          'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS', 
-          'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8;application/json',
+          'Access-Control-Allow-Origin'  : '*',
+          'Access-Control-Allow-Headers' : '*',
+          'Access-Control-Allow-Methods' :'GET,PUT,POST,DELETE,PATCH,OPTIONS', 
+          'Content-Type'                 : 'application/x-www-form-urlencoded; charset=UTF-8;application/json',
   }
 });
 
