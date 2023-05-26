a2c5e71fb18f24b6aaeb74ab5289c5cbcc66fc6d
Songonha <105191398+songonha@users.noreply.github.com>
2023-05-26 22:45:25 +0700

    Update server.js

--- client/src/server.js
+++ client/src/server.js
@@ -2,6 +2,11 @@ import axios from "axios";
 
 const server = axios.create({
   baseURL: "http://localhost:3042",
+  withCredentials: false,
+  headers: {
+          'Access-Control-Allow-Origin' : '*',
+          'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',   
+  }
 });
 
 export default server;
